53fb1192041620a2bb12f7826d4adf97f916b7f1
Zhu Jiongyao <zjywade@qq.com>
2016-04-28 14:31:26 +0800

    optimize arguments

--- saltMethods.py
+++ saltMethods.py
@@ -13,13 +13,13 @@ from logger import get_logger
 salt_logger = get_logger('salt')
 
 
-def get_minion_status(client, master_opts):
+def get_minion_status(salt_client, master_opts):
     '''
     Print the status of all known salt minions
     '''
     ret = {}
     try:
-        minions = client.cmd('*', 'test.ping', timeout=5)
+        minions = salt_client.cmd('*', 'test.ping', timeout=5)
     except SaltClientError as client_error:
         salt_logger.exception(
             'salt-master is down, Traceback info:\n %s', client_error)
@@ -33,24 +33,27 @@ def get_minion_status(client, master_opts):
     return ret
 
 
-def get_minion_up(client, master_opts):
-    ret = get_minion_status(client, master_opts).get('up', [])
+def get_minion_up(salt_client, master_opts):
+    ret = get_minion_status(salt_client, master_opts).get('up', [])
     return ret
 
 
-def get_minion_down(client, master_opts, removekeys=False):
-    ret = get_minion_status(client, master_opts).get('down', [])
+def get_minion_down(salt_client, master_opts, removekeys=False):
+    ret = get_minion_status(salt_client, master_opts).get('down', [])
     for minion in ret:
         if removekeys:
-            wheel = salt.wheel.Wheel(master_opts)
-            wheel.call_func('key.delete', match=minion)
+            # wheel = salt.wheel.Wheel(master_opts)
+            # wheel.call_func('key.delete', match=minion)
+            wheel = salt.wheel.WheelClient(master_opts)
+            wheel.cmd('key.delete', [minion])
     return ret
 
+
 if __name__ == '__main__':
-    client = salt.client.LocalClient()
+    salt_client = salt.client.LocalClient()
     master_opts = salt.config.client_config('/etc/salt/master')
-    # print get_minion_status(client, master_opts)
-    print get_minion_up(client, master_opts)
-    print get_minion_down(client, master_opts)
-    # print get_minion_down(client, master_opts, removekeys=True)
-    print get_minion_status(client, master_opts)
+    # print get_minion_status(salt_client, master_opts)
+    # print get_minion_up(salt_client, master_opts)
+    # print get_minion_down(salt_client, master_opts)
+    print get_minion_down(salt_client, master_opts, removekeys=True)
+    print get_minion_status(salt_client, master_opts)
