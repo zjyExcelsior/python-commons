1da4ad0ddafb28428f46f4cda486f67cd209317c
Zhu Jiongyao <zjywade@qq.com>
2016-05-03 14:15:13 +0800

    add cards and optimize

--- sqlalchemyMethods.py
+++ sqlalchemyMethods.py
@@ -1,15 +1,19 @@
 # coding: utf-8
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from contextlib import contextmanager
+from logger import get_logger
+
+sql_logger = get_logger('sql')
 
 engine = create_engine('mysql://root:123456@localhost/testdb', echo=False)
 Base = declarative_base()
 #Session = sessionmaker()
-#Session.configure(bind=engine)
+# Session.configure(bind=engine)
 Session = sessionmaker(bind=engine)
+
 
 @contextmanager
 def session_scope():
@@ -25,6 +29,7 @@ def session_scope():
     finally:
         session.close()
 
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -34,6 +39,19 @@ class User(Base):
 
     def __repr__(self):
         return '<User(name="%s", password="%s")>' % (self.name, self.password)
+
+
+class Card(Base):
+    __tablename__ = 'cards'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    owner_id = Column(Integer, ForeignKey('users.id'))
+    owner = relationship(
+        'User', backref=backref('cards', cascade='all, delete-orphan'))
+
+    def __repr__(self):
+        return '<Card(name="%s")>' % self.name
 
 
 def create_tables():
@@ -49,28 +67,48 @@ def add_user(session):
         session.add_all([
             User(name='zhujy', password='Zjy@2016'),
             User(name='tongcc', password='Tcc@2016')
-            ])
+        ])
+        session.flush()
+        card_zjy = Card(name='icbc', owner_id=user_zjy.id)
+        card_tcc = Card(name='abc', owner_id=user_tcc.id)
+        session.add_all([card_zjy, card_tcc])
+
 
 def delete_user(session, name):
     with session_scope() as session:
-        result = session.query(User).filter(User.name == name).one()
-        session.delete(result)
+        results = session.query(User).filter(User.name == name).all()
+        for result in results:
+            session.delete(result)
+
 
 def update_user(session, name, new_password):
     with session_scope() as session:
-        result = session.query(User).filter(User.name == name).one()
-        result.password = new_password
-
-def select_user(session, name):
-    with session_scope() as session:
         results = session.query(User).filter(User.name == name).all()
+        for result in results:
+            result.password = new_password
+
+
+def select_user_cards(session, user_name):
+    with session_scope() as session:
+        users = session.query(User).filter(User.name == user_name).all()
         # session.expunge_all()
-    return map(session.merge, results)
+    if not users:
+        sql_logger.info('No row was found for %s', user_name)
+        return users
+    users = map(session.merge, users)
+    ret = []
+    for user in users:
+        cards = user.cards
+        ret.append('user: %s, id: %s, cards: %s' % (user, user.id, cards))
+    return ret
 
 if __name__ == '__main__':
     create_tables()
     session = Session()
     # add_user(session)
     # delete_user(session, 'zhujiongyao')
+    # delete_user(session, 'tongchenchen')
     # update_user(session, 'tongchenchen', 'tcc@2016')
-    print select_user(session, 'tongchenchen')
+    # print select_user(session, 'tongchenchen')
+    print select_user_cards(session, 'zhujydd')
+    print select_user_cards(session, 'zhujy')
