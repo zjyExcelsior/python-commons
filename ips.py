2e95bdb96c37e689a5bcf1d975449d7020bf6f6b
Zhu Jiongyao <zjywade@qq.com>
2016-05-04 20:39:31 +0800

    add is_valid_url() method

--- ips.py
+++ ips.py
@@ -3,6 +3,7 @@
 some method about ip
 '''
 import socket
+import urlparse
 
 
 def get_ip_ranges(begin_ip, end_ip=''):
@@ -24,8 +25,14 @@ def get_sorted_ip_list(ip_list):
     '''
     return sorted(ip_list, key=socket.inet_aton)
 
+def is_valid_url(url):
+    parts = urlparse.urlparse(url)
+    return parts.scheme in ('http', 'https')
+
 if __name__ == '__main__':
     print get_ip_ranges('192.168.1.22')  # 192.168.1.22
     print get_ip_ranges('192.168.1.22', '192.168.3.33')  # 192.168.1-3.22-33
     ip_list = ['192.168.1.33', '10.5.1.3', '10.5.2.4', '202.98.96.68', '133.120.1.1']
     print get_sorted_ip_list(ip_list)
+    print is_valid_url('http://www.google.com')
+    print is_valid_url('www.google.com')
